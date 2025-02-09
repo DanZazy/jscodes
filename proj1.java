fcaef0aecea0f60e3ff40eb4f98790a8e4ac0454
Danylo Zazymko <zaz.deny@gmail.com>
2025-02-09 16:35:25 +0200

    second commit

--- proj1.java
+++ proj1.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class proj1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Як вас звати?");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String userName = input.readLine();
+
+        int date = 2024;
+        System.out.println("Скільки вам років?");
+        int age = Integer.parseInt(input.readLine());
+           
+        System.out.println("Введіть масу тіла:");
+        float mass = Float.parseFloat(input.readLine());
+
+
+        System.out.println("Привіт, " + userName + "!, " + "що народився в " + (date-age) + " році" + " та важить " + mass + "  кілограми" + "!");
     }
-
-
 }
-
