0bb22986e275b9e81538cc089f756d6b5d7f591b
DanZazy <zaz.deny@gmail.com>
2025-02-12 16:58:00 +0200

    Add files via upload

---
+++ Gradus.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Gradus {
+    public static void main (String[] args) throws IOException  {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please, enter temperature in Celsius: ");
+        
+        Float T = Float.parseFloat(input.readLine());
+
+        System.out.println("Calculated temperature in Fahrenheits: " + (T * 9 / 5 + 32));
+
+    }
+}
+
