54384214a14646f721a67ba62e9c40529d9e3702
Danylo Zazymko <zaz.deny@gmail.com>
2025-03-17 13:39:15 +0200

    minor changes

--- Lab2-3/Task1.java
+++ Lab2-3/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter c value (negative): ");
+        System.out.print("Enter c value (negative): \n");
         double c = sc.nextDouble();
         
         if (c >= 0) {
@@ -16,14 +16,14 @@ public class Task1 {
         double ha = 0.25 * Math.abs(c);
         double a;
 
-        for (a = c; a < 0; a += ha) { // Рухаємося до 0
-            double denominator = Math.pow(Math.sin(a), 2);
+        for (a = c; a < 0; a += ha) {
+            double d = Math.pow(Math.sin(a), 2);
             
-            if (denominator > 0) { 
-                double y = a / Math.log(denominator);
+            if (d > 0) { 
+                double y = a / Math.log(d);
                 System.out.println("a: " + a + ", y: " + y);
             } else {
-                System.out.println("Error: Denominator is zero or negative.");
+                System.out.println("Error: d value is zero or negative.");
             }
             if (a + ha >= 0) break;
 
---
+++ Lab2-4/arr.java
@@ -0,0 +1,7 @@
+
+public class arr {
+    public static void main(String[] args) {
+
+        
+    }
+}
